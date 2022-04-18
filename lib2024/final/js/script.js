88e0529866969173e8670c37f8c1974c586d5a29
crai0251 <97844964+crai0251@users.noreply.github.com>
2022-04-18 11:50:49 -0400

    tenth commit

---
+++ lib2024/final/js/script.js
@@ -0,0 +1,43 @@
+window.onload = init;
+
+function init() {
+
+	document.querySelector('.ham').onclick = showHideMobileMenu;
+	document.querySelector('#submit').onclick = showformValues;
+
+	$('#form').submit(function (e) {
+   		 e.preventDefault();
+   		 var form = this;
+   		 $(".overlay-container").fadeIn(800, function(){
+   		 		showformValues(form);
+   		 		$('.overlay-container').delay(400).fadeOut(400);
+   		 });
+	});
+
+}
+
+function showHideMobileMenu() {
+
+	var mobileNav = document.querySelector('.mobile-nav');
+
+	if(mobileNav.style.display=="block") {
+		mobileNav.style.display="none";
+	} else {
+		mobileNav.style.display="block";
+	}
+}
+
+function showformValues(form){
+
+	var formValues = $(form).serializeArray(); 
+		
+	$.each(formValues, function(index, field){
+
+
+		$("#results").find("#"+field.name+"_result").text(field.value);
+
+		if(field.name=="email"){
+			$("#results").find("#"+field.name+"_result").attr("href", "mailto:"+field.value);
+		}
+	});				
+}
